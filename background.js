7d1bc30b8b6617d73e33cd8487f50200316c7ffa
Razvan Soare <soare.razvan94@gmail.com>
2023-05-30 22:02:11 +0100

    Improve styles and save blocked sites as tags instead of textarea

--- background.js
+++ background.js
@@ -15,98 +15,37 @@ const resourceTypes = [
   "webtransport",
   "xmlhttprequest",
 ];
+
 function setupBlocker() {
-  let blockedWebsites = [];
-  let startTime = null;
-  let endTime = null;
   // Get blocked websites from storage
-  chrome.storage.sync.get("blockedWebsites", (data) => {
-    const body = JSON.parse(data.blockedWebsites);
-    if (body) {
-      blockedWebsites = body.blockedWebsites.split(",").filter((x) => !!x);
-      startTime = body.startTime;
-      endTime = body.endTime;
-    }
-    let action = "allow";
-    if (startTime && endTime) {
-      // add dynamic rules only between 8am and 5pm
-      const now = new Date();
-      const start = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        startTime.split(":")[0],
-        startTime.split(":")[1]
-      );
-      const end = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        endTime.split(":")[0],
-        endTime.split(":")[1]
-      );
-      if (now > start && now < end) {
-        action = "block";
-      }
-    }
-    chrome.declarativeNetRequest.updateDynamicRules({
-      addRules: blockedWebsites.map((website, index) => ({
-        id: 1000 + index,
-        priority: 1,
-        action: {
-          type: action,
-        },
-        condition: {
-          urlFilter: website,
-          resourceTypes,
-        },
-      })),
-      removeRuleIds: Array.from({ length: 100 }, (_, i) => i + 1).map(
-        (_, index) => 1000 + index
-      ),
+  chrome.storage.sync.get("blockedWebsitesList", ({ blockedWebsitesList }) => {
+    chrome.storage.sync.get("active", ({ active }) => {
+      const action = active ? "block" : "allow";
+
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: blockedWebsitesList.map((website, index) => ({
+          id: 1000 + index,
+          priority: 1,
+          action: {
+            type: action,
+          },
+          condition: {
+            urlFilter: website,
+            resourceTypes,
+          },
+        })),
+        removeRuleIds: Array.from({ length: 100 }, (_, i) => i + 1).map(
+          (_, index) => 1000 + index
+        ),
+      });
     });
   });
 }
 
 setupBlocker();
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.method === "rerender") {
-    console.log("rerendering");
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.method === "toggle") {
     setupBlocker();
   }
 });
-
-// function tick() {
-//     // Re-calculate the timestamp for the next day
-//     let next = new Date(Date.now() + 24 * 60 * 60 * 1000);
-
-//     // Adjust the timestamp if you want to run the code
-//     // around the same time of each day (e.g. 10:00 am)
-//     next.setHours(10);
-//     next.setMinutes(0);
-//     next.setSeconds(0);
-//     next.setMilliseconds(0);
-
-//     // Save the new timestamp
-//     localStorage.savedTimestamp = next.getTime();
-
-//     // Run the function
-//     doSomething();
-// }
-
-// function checkTimestamp() {
-//     if (localStorage.savedTimestamp) {
-//         let timestamp = parseInt(localStorage.savedTimestamp);
-
-//         if (Date.now() >= timestamp) {
-//             tick();
-//         }
-//     } else {
-//         // First time running
-//         tick();
-//     }
-// }
-
-// // Check every minute
-// setInterval(checkTimestamp, 60000);
--- popup.js
+++ popup.js
@@ -1,52 +1,128 @@
+const DEFAULT_WEBSITES = [
+  "https://www.facebook.com/",
+  "https://www.youtube.com/",
+  "https://www.reddit.com/",
+  "https://www.instagram.com/",
+  "https://www.twitter.com/",
+  "https://www.tiktok.com/",
+  "https://www.netflix.com/",
+  "https://www.hulu.com/",
+  "https://www.amazon.com/",
+  "https://www.ebay.com/",
+];
+function setIconState({ active }) {
+  if (active === true) {
+    document.querySelector("#stop-enabled").classList.remove("hidden");
+    document.querySelector("#stop-disabled").classList.add("hidden");
+    chrome.action.setIcon({
+      path: "./images/icons/favicon.ico",
+    });
+  } else {
+    chrome.action.setIcon({
+      path: "./images/icons/favicon-disabled.ico",
+    });
+    document.querySelector("#stop-enabled").classList.add("hidden");
+    document.querySelector("#stop-disabled").classList.remove("hidden");
+  }
+}
+
+function formatLabel(text) {
+  return text
+    .replace("https://www.", "")
+    .replace("http://www.", "")
+    .replace("https://", "")
+    .replace("http://", "");
+}
+
+function recalculateActiveTags() {
+  const blockedSites = document.querySelectorAll(".blocked-site-tag span");
+  const blockedWebsitesList = Array.from(blockedSites).map(
+    (site) => site.innerHTML
+  );
+
+  chrome.storage.sync.set(
+    {
+      blockedWebsitesList,
+    },
+    function () {
+      chrome.runtime.sendMessage({ method: "toggle" });
+    }
+  );
+}
+
+function addTag(text) {
+  const parent = document.querySelector("#blocked-sites-wrapper");
+  const div = document.createElement("div");
+  const tagId = Math.random().toString(36).substring(7);
+  div.id = tagId;
+  div.classList.add("blocked-site-tag");
+  div.innerHTML = `<span>${formatLabel(text)}</span>`;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.innerHTML = "X";
+  deleteButton.addEventListener("click", function () {
+    removeTag(tagId);
+    recalculateActiveTags();
+  });
+  div.appendChild(deleteButton);
+  parent.appendChild(div);
+}
+function removeTag(tagId) {
+  const parent = document.querySelector("#blocked-sites-wrapper");
+  const div = document.getElementById(tagId);
+  parent.removeChild(div);
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
   async function init() {
-    // INIT
-    let blockedWebsites = [];
-    let startTime = null;
-    let endTime = null;
-    await chrome.storage.sync.get("blockedWebsites", (data) => {
-      const body = JSON.parse(data.blockedWebsites);
-      if (body) {
-        blockedWebsites = body.blockedWebsites;
-        startTime = body.startTime;
-        endTime = body.endTime;
-      }
-      let blockedWebsitesObject = document.querySelector("#block-list");
+    await chrome.storage.sync.get(
+      "blockedWebsitesList",
+      ({ blockedWebsitesList }) => {
+        const siteElements = document.querySelectorAll(".blocked-site-tag");
+        if (siteElements?.length > 0) {
+          // we already populated the items
+          return null;
+        }
 
-      let startTimeObject = document.querySelector("#start-time");
-      let endTimeObject = document.querySelector("#end-time");
+        if (blockedWebsitesList?.length > 0) {
+          blockedWebsitesList.forEach((site) => addTag(site));
+        }
+      }
+    );
 
-      if (blockedWebsites && blockedWebsites.length > 0) {
-        blockedWebsitesObject.value = blockedWebsites.replace(/,/g, "\n");
-      }
-      if (startTime) {
-        startTimeObject.value = startTime;
-      }
-      if (endTime) {
-        endTimeObject.value = endTime;
-      }
+    chrome.storage.sync.get("active", ({ active }) => {
+      setIconState({ active });
     });
   }
   await init();
 
   document
-    .querySelector("#myForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      var blockedWebsites = document.querySelector("#block-list").value || "";
-      var startTime = document.querySelector("#start-time").value || null;
-      var endTime = document.querySelector("#end-time").value || null;
-      const body = {
-        startTime,
-        endTime,
-        blockedWebsites: blockedWebsites.replace(/\n/g, ","),
-      };
-      chrome.storage.sync.set(
-        { blockedWebsites: JSON.stringify(body) },
-        function () {
-          console.log("Value is set to " + JSON.stringify(body, null, 2));
-          chrome.runtime.sendMessage({ method: "rerender" });
-        }
-      );
+    .querySelector("#add-new-site")
+    .addEventListener("click", function () {
+      const input = document.querySelector("input#new-site");
+      if (input.value === "") {
+        return;
+      }
+      addTag(input.value);
+
+      input.value = "";
+      recalculateActiveTags();
     });
+
+  document.querySelector("#start").addEventListener("click", function () {
+    chrome.storage.sync.set({ active: true }, function () {
+      chrome.runtime.sendMessage({ method: "toggle" });
+    });
+
+    setIconState({ active: true });
+  });
+
+  document.querySelector("#stop").addEventListener("click", function () {
+    chrome.storage.sync.set({ active: false }, function () {
+      chrome.runtime.sendMessage({ method: "toggle" });
+    });
+
+    setIconState({ active: false });
+  });
 });
